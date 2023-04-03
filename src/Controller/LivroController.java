bfe2ae41d138878130ee24aa978066c02cb9cca9
Carlos Santana <carlosefdesantana@gmail.com>
2023-04-03 20:37:08 -0300

    feat: CRUD e Teste para exemplo.

---
+++ src/Controller/LivroController.java
@@ -0,0 +1,48 @@
+package Controller;
+
+import Model.Livro;
+
+public class LivroController {
+    private Livro[] livros;
+
+    public LivroController(){
+        livros = new Livro[10];
+    }
+
+    public void cadastrarLivro(Livro livro){
+        for(int i = 0; i < livros.length; i++){
+            if(livros[i] == null){
+                livros[i] = livro;
+                break;
+            }
+        }
+    }
+
+    public Livro[] listarLivros(){
+        return livros;
+    }
+
+    public Livro buscarLivro(String nome){
+        for(int i = 0; i < livros.length; i++){
+            if(livros[i] != null && livros[i].getNome().equals(nome)){
+                return livros[i];
+            }
+        }
+        return null;
+    }
+
+    public void removerLivro(String nome){
+        for(int i = 0; i < livros.length; i++){
+            if(livros[i] != null && livros[i].getNome().equals(nome)){
+                livros[i] = null;
+                break;
+            }
+        }
+    }
+
+    // Cria um livro
+    public Livro criarLivro(String nome, String descricao, int quantidade){
+        Livro livro = new Livro(nome, descricao, quantidade);
+        return livro;
+    }
+}
---
+++ src/View/Teste.java
@@ -0,0 +1,27 @@
+package View;
+
+import Controller.LivroController;
+import Model.Livro;
+
+public class Teste {
+    public static void main(String[] args) {
+        LivroController livroController = new LivroController();
+
+        // Cria um livro
+        Livro livro = livroController.criarLivro("Livro 1", "Descrição do livro 1", 10);
+
+        // Adiciona o livro no banco de dados
+        livroController.cadastrarLivro(livro);
+
+        // Lista todos os livros cadastrados
+        Livro[] livros = livroController.listarLivros();
+        for(int i = 0; i < livros.length; i++){
+            if(livros[i] != null){
+                System.out.println("Nome: " + livros[i].getNome());
+                System.out.println("Descrição: " + livros[i].getDescricao());
+                System.out.println("Quantidade: " + livros[i].getQuantidade());
+                System.out.println("----------------------------");
+            }
+        }
+    }
+}
